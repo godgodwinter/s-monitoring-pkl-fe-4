2c0629821d77b0a6114fa1c546b73aca8914cdb8
godgodwinter <kakadz.pro@gmail.com>
2022-04-29 20:44:08 +0700

    update crud tapel tambahkan status

--- src/views/admin/tapel/Index.vue
+++ src/views/admin/tapel/Index.vue
@@ -52,6 +52,11 @@ const columns = [
     field: "nama",
     type: "String",
   },
+  {
+    label: "Status",
+    field: "status",
+    type: "String",
+  },
 ];
 
 const getDataId = async () => {
@@ -106,6 +111,7 @@ const doStoreData = async (d) => {
     if (dataId) {
       const response = await Api.put(`admin/tapel/${dataId}`, {
         nama: d.nama,
+        status: d.status,
       });
 
       Toast.success("Success", "Data Berhasil diupdate!");
@@ -114,6 +120,7 @@ const doStoreData = async (d) => {
     }
     const response = await Api.post("admin/tapel/store", {
       nama: d.nama,
+      status: d.status,
     });
 
     getData();
@@ -205,6 +212,38 @@ function resetForm() {
                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
                       required
                     />
+                    <div class="text-xs text-red-600 mt-1">{{ errors.name }}</div>
+                  </div>
+                </div>
+                <div class="grid grid-cols-1 gap-6 mt-4">
+                  <div class="col-span-6 sm:col-span-3">
+                    <label for="name" class="text-sm font-medium text-gray-900 block mb-1"
+                      >Status</label
+                    >
+                    <div class="w-full flex gap-4">
+                      <label class="inline-flex items-center mt-3">
+                        <Field
+                          v-model="dataDetail.status"
+                          value="Aktif"
+                          type="radio"
+                          name="status"
+                          ref="status"
+                          class="form-radio h-5 w-5 text-gray-600"
+                          checked
+                        /><span class="ml-2 text-gray-700">Aktif</span>
+                      </label>
+
+                      <label class="inline-flex items-center mt-3">
+                        <Field
+                          v-model="dataDetail.status"
+                          value="Nonaktif"
+                          type="radio"
+                          name="status"
+                          ref="status"
+                          class="form-radio h-5 w-5 text-red-600"
+                        /><span class="ml-2 text-gray-700">Nonaktif</span>
+                      </label>
+                    </div>
                     <div class="text-xs text-red-600 mt-1">{{ errors.name }}</div>
                   </div>
                 </div>
